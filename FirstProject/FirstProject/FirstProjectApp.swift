5e09b1a534163d667ef1b81a842fac8e8d4bb208
Ahmet Mucahid Bozkurt <ahmetmb46@gmail.com>
2022-05-07 00:07:35 +0300

    FirstProject

---
+++ FirstProject/FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Ahmet Mucahid Bozkurt on 7.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
