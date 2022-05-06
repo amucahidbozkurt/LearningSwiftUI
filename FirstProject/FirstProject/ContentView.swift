969d78c1a75070dbb99549e424aa296627a9ad69
Ahmet Mucahid Bozkurt <ahmetmb46@gmail.com>
2022-05-07 00:56:25 +0300

    VStack, HStack and ZStack

--- FirstProject/FirstProject/ContentView.swift
+++ FirstProject/FirstProject/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .bold().italic()
+                .font(.title)
+                .padding(15)
+                .background(Color.yellow)
+                .cornerRadius(30)
+            
+            Text("Second Text")
+                .font(.headline.italic())
+                .foregroundColor(.blue)
+                .padding(.bottom)
+            
+            HStack {
+                Text("Text 1")
+                    .foregroundColor(.brown)
+                Text("Text 2")
+                    .foregroundColor(.indigo)
+                Text("Text 3")
+                    .foregroundColor(.white)
+            }.padding().background(Color.green)
+            
+            // TODO: Use ZStack for if you want to decide positions
+            ZStack {
+                Text("ZStack-1")
+                Text("ZStack-2")
+            }
+            
+            ZStack {
+                Image("kadikoy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Text("FENERBAHÇE")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .background(Color.yellow)
+            }
+        }
     }
 }
 
