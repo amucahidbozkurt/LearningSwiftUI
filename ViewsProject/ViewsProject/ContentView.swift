3950bdafbd9aeff1bc8531190f69693737db259d
Ahmet Mucahid Bozkurt <ahmetmb46@gmail.com>
2022-05-07 14:59:06 +0300

    ViewsProject

---
+++ ViewsProject/ViewsProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewsProject
+//
+//  Created by Ahmet Mucahid Bozkurt on 7.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewsProject/ViewsProject/ViewsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsProjectApp.swift
+//  ViewsProject
+//
+//  Created by Ahmet Mucahid Bozkurt on 7.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
